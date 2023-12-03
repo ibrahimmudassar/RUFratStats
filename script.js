270904b6b180c9c4892b644c62f7889b5cc34b65
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2023-12-03 09:28:06 +0000

    adding datatables to website

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 $(document).ready(function () {
   // DataTable
-  $("#mainTable").DataTable({
-    order: [[1, "desc"]],
+  $("#percentile").DataTable({
+    order: [[0, "asc"]],
 
     scrollY: "98vh",
     scrollCollapse: true,
-
-    lengthMenu: [
-      [15, 50, 100, -1],
-      [15, 50, 100, "All"],
-    ],
 
     searchBuilder: true,
 
@@ -23,5 +18,50 @@ $(document).ready(function () {
         return $.csv.toObjects(csvdata);
       },
     },
+    columns: [
+      { data: "Rank" },
+      { data: "Chapter" },
+      { data: "Chapter Type" },
+      { data: "Total Chapter GPA" },
+      { data: "Average Hours Per Member" },
+      { data: "Annual Report Score" },
+      { data: "Charitable Donations Per Member" },
+      { data: "Total" },
+    ],
+  });
+
+  $("#chapters_raw").DataTable({
+    order: [[0, "asc"]],
+
+    scrollY: "98vh",
+    scrollCollapse: true,
+
+    searchBuilder: true,
+
+    responsive: true,
+
+    deferRender: true,
+    ajax: {
+      url: "https://raw.githubusercontent.com/ibrahimmudassar/RUFratStats/main/S23raw.csv",
+      dataType: "text",
+      dataSrc: function (csvdata) {
+        return $.csv.toObjects(csvdata);
+      },
+    },
+    columns: [
+      { data: "Chapter" },
+      { data: "Total Chapter Size" },
+      { data: "New Member Class Size" },
+      { data: "Total Chapter GPA" },
+      { data: "New Member GPA" },
+      { data: "Total Service Hours" },
+      { data: "Average Hours Per Member" },
+      { data: "Charitable Donations" },
+      { data: "Charitable Donations Per Member" },
+      { data: "Annual Report Score" },
+      { data: "Star Ranking" },
+      { data: "Chapter Type" },
+      { data: "Semester" },
+    ],
   });
 });
